9b83ec2bad42cc41b5870d09bcb79654ad867f8d
Sergey <sergsmile29@gmail.com>
2024-08-01 12:26:23 +0300

    conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import Header from './components/Header/Header';
 import Message from './components/Message/Message';
 
 const App = () => {
+  const isOnline = true;
+  const isLoading = true;
+  const isError = false;
+  const isOpen = true;
   return (
     <>
       <Header />
-
-      <Message title='Maria' text='Hello world' />
-      <Message title='Helen' text='Куплю холодильник!' />
-      <Message title='Ivan' text='Супер товар!' />
+      {/* {isOnline && <h1>Welcome back!</h1>}
+      {isOnline || <h1>Offline</h1>} */}
+      {isLoading && <h2>Loading...</h2>}
+      {isError && <h2>Error</h2>}
+      {isOpen && <h2>Open modal</h2>}
+      {isOnline ? <h1>Welcome back!</h1> : <h1>Offline</h1>}
+      <Message title='Maria' text='Hello world' online={true} />
+      <Message title='Helen' text='Куплю холодильник!' online={false} />
+      <Message title='Ivan' text='Супер товар!' online={false} />
 
       <Footer />
     </>
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,10 @@
-const Message = ({ title = 'DefaultUser', text }) => {
+const Message = ({ title = 'DefaultUser', text, online }) => {
   console.log(title);
   return (
     <div>
       <h3>{title}</h3>
       <p>{text}</p>
+      <p>Status: {online ? 'active' : 'disabled'} </p>
     </div>
   );
 };
