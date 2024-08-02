b9426745c0aad1957a4f9a51fdc2a2b5bdfcff99
Sergey <sergsmile29@gmail.com>
2024-08-02 10:32:10 +0300

    clsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,13 @@
-import css from './List.module.css';
+import clsx from 'clsx';
+import s from './List.module.css';
 
 export const List = ({ data, title }) => {
+  const isActive = true;
   return (
-    <div className={css.wrapper}>
-      <h2 className={css.title}>{title}</h2>
-      <ul>
+    <div className={s.wrapper}>
+      <h2 className={s.title}>{title}</h2>
+      {/* <ul className={`${s.list} ${s.padding} ${isActive && s.active}`}> */}
+      <ul className={clsx(s.list, s.padding, isActive && s.active)}>
         {data.map(item => (
           <li key={item.id}>{item.title}</li>
         ))}
