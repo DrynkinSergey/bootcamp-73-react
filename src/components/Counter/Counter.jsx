4c45e1fbbda05d27ea01811bdb3e977b131e6070
Sergey <sergsmile29@gmail.com>
2024-08-05 10:14:02 +0300

    events

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,18 +1,35 @@
-import s from './Counter.module.css'
+import s from './Counter.module.css';
 
 export const Counter = () => {
-	return (
-		<div className={s.flexContainer}>
-			<div className={s.wrapper}>
-				<h1>{1}</h1>
+  const handleMinusClick = () => {
+    // e.target.innerHTML = 'CLICK BTN';
+    // console.log({ x: e.pageX, y: e.pageY });
+    console.log('Welcome!');
+  };
 
-				<input />
-				<div className={s.flex}>
-					<button className='btn'>minus</button>
-					<button className='btn'>reset</button>
-					<button className='btn'>plus</button>
-				</div>
-			</div>
-		</div>
-	)
-}
+  const handleResetClick = (event, name) => {
+    console.log(`Hello, ${name}`);
+    console.log(event);
+  };
+
+  return (
+    <div className={s.flexContainer}>
+      <div className={s.wrapper}>
+        <h1>{1}</h1>
+
+        <input />
+        <div className={s.flex}>
+          <button onClick={handleMinusClick} className='btn'>
+            minus
+          </button>
+          <button onClick={e => handleResetClick(e, 'Alex')} className='btn'>
+            reset
+          </button>
+          <button onClick={() => console.log('Hello, user!')} className='btn'>
+            plus
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
