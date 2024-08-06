691e82c80d4c6af6fa2d70dcb99124e1e2e6577f
Sergey <sergsmile29@gmail.com>
2024-08-06 10:59:29 +0300

    clenup function with useEffect

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,40 @@
+import { useEffect } from 'react';
 import s from './Modal.module.css';
-const Modal = ({ children, title = 'Default modal' }) => {
+const Modal = ({ onClose, children, title = 'Default modal' }) => {
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handlePressKey = e => {
+      console.log(e.key);
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', handlePressKey);
+
+    return () => {
+      document.removeEventListener('keydown', handlePressKey);
+      console.log('Unmount modal');
+    };
+  }, [onClose]);
+
   return (
-    <div className={s.wrapper}>
+    <section onClick={handleBackdropClick} className={s.wrapper}>
       <div className={s.content}>
         <>
           <h1>{title}</h1>
           <hr />
         </>
-        <button className={s.closeBtn}>×</button>
+        <button onClick={onClose} className={s.closeBtn}>
+          ×
+        </button>
         {children}
       </div>
-    </div>
+    </section>
   );
 };
 
