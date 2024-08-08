0e3b52ce6db1d45dbeade7b9c5a7c64c49c9097f
Sergey <sergsmile29@gmail.com>
2024-08-08 10:14:26 +0300

    uncontrolled form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,30 @@
 import s from './LoginForm.module.css';
 const LoginForm = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+
+    const username = form.elements.username.value;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    console.log({ username, email, password });
+    form.reset();
+  };
+
   return (
     <div className={s.formWrapper}>
-      <form className={s.form}>
+      <form onSubmit={handleSubmit} className={s.form}>
         <label className={s.label}>
           <span>Name:</span>
-          <input placeholder='Enter name...' className={s.input} />
+          <input placeholder='Enter name...' className={s.input} name='username' />
         </label>
         <label className={s.label}>
           <span>Email:</span>
-          <input placeholder='Enter email...' className={s.input} />
+          <input placeholder='Enter email...' className={s.input} name='email' />
         </label>
         <label className={s.label}>
           <span>Password:</span>
-          <input placeholder='Enter password...' className={s.input} />
+          <input placeholder='Enter password...' className={s.input} name='password' />
         </label>
         <button className={s.btn}>Login</button>
       </form>
