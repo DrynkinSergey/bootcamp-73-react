59569cc31c112a9f86c724fb79f3e4d1da41edb5
Sergey <sergsmile29@gmail.com>
2024-08-09 12:02:50 +0300

    valid field

--- src/components/Formik/Register.jsx
+++ src/components/Formik/Register.jsx
@@ -3,6 +3,7 @@ import s from './Register.module.css';
 import { nanoid } from 'nanoid';
 
 import * as Yup from 'yup';
+import clsx from 'clsx';
 
 const Register = () => {
   // 1. Встановити Formik
@@ -31,54 +32,58 @@ const Register = () => {
   return (
     <div className={s.formWrapper}>
       <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={registerSchema}>
-        <Form className={s.form}>
-          <label>
-            <span>Name:</span>
-            <Field type='text' name='name' />
-            <ErrorMessage className={s.error} name='name' component='div' />
-          </label>
-          <label>
-            <span>Email:</span>
-            <Field type='email' name='email' />
-            <ErrorMessage className={s.error} name='email' component='div' />
-          </label>
-          <label>
-            <span>Password:</span>
-            <Field type='password' name='password' />
-            <ErrorMessage className={s.error} name='password' component='div' />
-          </label>
-          <label>
-            <span>About:</span>
-            <Field as='textarea' name='about' />
-          </label>
-          <label>
-            <span>Country:</span>
-            <Field as='select' name='country'>
-              <option value=''>Default value</option>
-              <option value='Ukraine'>Ukraine</option>
-              <option value='Brazil'>Brazil</option>
-              <option value='Comboja'>Comboja</option>
-            </Field>
-            <ErrorMessage className={s.error} name='country' component='div' />
-          </label>
-          <label className={s.flex}>
-            <Field type='radio' name='gender' value='male' />
-            <span>Male</span>
-          </label>
-          <label className={s.flex}>
-            <Field type='radio' name='gender' value='female' />
-            <span>Female</span>
-          </label>
+        {({ errors, values }) => {
+          return (
+            <Form className={s.form}>
+              <label>
+                <span>Name:</span>
+                <Field className={clsx(errors.name && s.invalid, !errors.name && values.name && s.valid)} type='text' name='name' />
+                <ErrorMessage className={s.error} name='name' component='div' />
+              </label>
+              <label>
+                <span>Email:</span>
+                <Field type='email' name='email' />
+                <ErrorMessage className={s.error} name='email' component='div' />
+              </label>
+              <label>
+                <span>Password:</span>
+                <Field type='password' name='password' />
+                <ErrorMessage className={s.error} name='password' component='div' />
+              </label>
+              <label>
+                <span>About:</span>
+                <Field as='textarea' name='about' />
+              </label>
+              <label>
+                <span>Country:</span>
+                <Field as='select' name='country'>
+                  <option value=''>Default value</option>
+                  <option value='Ukraine'>Ukraine</option>
+                  <option value='Brazil'>Brazil</option>
+                  <option value='Comboja'>Comboja</option>
+                </Field>
+                <ErrorMessage className={s.error} name='country' component='div' />
+              </label>
+              <label className={s.flex}>
+                <Field type='radio' name='gender' value='male' />
+                <span>Male</span>
+              </label>
+              <label className={s.flex}>
+                <Field type='radio' name='gender' value='female' />
+                <span>Female</span>
+              </label>
 
-          <label className={s.flex}>
-            <Field type='checkbox' name='agree' />
-            <span>I confirm this rules</span>
-          </label>
-          <div className={s.btns}>
-            <button type='submit'>Register</button>
-            <button type='reset'>Reset</button>
-          </div>
-        </Form>
+              <label className={s.flex}>
+                <Field type='checkbox' name='agree' />
+                <span>I confirm this rules</span>
+              </label>
+              <div className={s.btns}>
+                <button type='submit'>Register</button>
+                <button type='reset'>Reset</button>
+              </div>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
