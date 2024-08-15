6237d1e9919f85962557c9e97fbeaaafc50de073
Sergey <sergsmile29@gmail.com>
2024-08-15 10:07:55 +0300

    browserRouter, routes, route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+
 const App = () => {
-  return <div>Routing</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<h2>Home</h2>} />
+        <Route path='/about' element={<h2>About</h2>} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
-import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 import s from './Header.module.css';
 const Header = () => {
   return (
     <header className='flex items-center justify-between px-4 py-1 bg-teal-500 text-white'>
       <h2>Logo</h2>
       <ul className={s.list}>
-        <li>Home</li>
-        <li className={clsx('1', s.hello)}>About</li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
       </ul>
     </header>
   );
