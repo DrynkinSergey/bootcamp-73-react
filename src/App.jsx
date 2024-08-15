172899ddc6b38f4c087c7cc5366a76c08d61b335
Sergey <sergsmile29@gmail.com>
2024-08-15 11:00:40 +0300

    NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='about' element={<About />} />
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 };
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
-    <div>
-      <h1 className='text-center font-bold '>Not found page</h1>
+    <div className='flex justify-center items-center min-h-screen flex-col gap-2'>
+      <img src='https://devstarter.technology/img/articles/fullsize/230915180444.png' alt='notFound' />
+      <h1 className='text-center font-bold '>
+        <Link to='/'>Home</Link>
+      </h1>
     </div>
   );
 };
