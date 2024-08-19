919a5722d7d5fe89d60bfe1568366581a21b7497
Sergey <sergsmile29@gmail.com>
2024-08-19 10:59:29 +0300

    counter redux

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const counter = useSelector(state => state.counter.counter);
+  const step = useSelector(state => state.counter.step);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+  const handleChangeStep = e => {
+    dispatch({ type: 'CHANGE_STEP', payload: +e.target.value });
+  };
+  const reset = () => {
+    dispatch({ type: 'RESET' });
+  };
 
   return (
     <div className='flex items-center justify-center min-h-screen'>
       <div className='flex items-center  justify-center gap-4'>
-        <button className='px-4 py-2 bg-teal-500 text-white font-bold' onClick={() => setCount(prev => prev - 1)}>
+        <input value={step} onChange={handleChangeStep} className='input border-2 border-black' type='text' />
+
+        <button className='px-4 py-2 bg-teal-500 text-white font-bold' onClick={handleDecrement}>
           Decrement
         </button>
-        <h1 className='text-3xl'>{count}</h1>
-        <button className='px-4 py-2 bg-teal-500 text-white font-bold' onClick={() => setCount(prev => prev + 1)}>
+        <h1 className='text-3xl'>{counter}</h1>
+        <button className='px-4 py-2 bg-teal-500 text-white font-bold' onClick={handleIncrement}>
           Increment
+        </button>
+        <button className='px-4 py-2 bg-teal-500 text-white font-bold' onClick={reset}>
+          RESET
         </button>
       </div>
     </div>
---
+++ src/redux/counter/reducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+  counter: 10,
+  step: 1,
+};
+
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        counter: state.counter + state.step,
+      };
+    }
+    case 'DECREMENT':
+      return {
+        ...state,
+        counter: state.counter - state.step,
+      };
+    case 'CHANGE_STEP':
+      return {
+        ...state,
+        step: action.payload,
+      };
+    case 'RESET':
+      return {
+        ...state,
+        step: 1,
+        counter: 0,
+      };
+    default:
+      return state;
+  }
+};
