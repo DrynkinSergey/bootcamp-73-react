de76e7aa6148df556d0cadabe9fb50280a940ddb
Sergey <sergsmile29@gmail.com>
2024-08-20 10:27:28 +0300

    createReducer

--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,35 +1,52 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { changeStep, decrement, increment, reset } from './actions';
 
 const initialState = {
-  counter: 10,
+  counter: 0,
   step: 1,
 };
 
-export const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case increment.type: {
-      return {
-        ...state,
-        counter: state.counter + state.step,
-      };
-    }
-    case decrement.type:
-      return {
-        ...state,
-        counter: state.counter - state.step,
-      };
-    case changeStep.type:
-      return {
-        ...state,
-        step: action.payload,
-      };
-    case reset.type:
-      return {
-        ...state,
-        step: 1,
-        counter: 0,
-      };
-    default:
-      return state;
-  }
-};
+export const counterReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(increment, (state, action) => {
+      state.counter += state.step;
+    })
+    .addCase(decrement, (state, action) => {
+      state.counter -= state.step;
+    })
+    .addCase(reset, (state, action) => {
+      return initialState;
+    })
+    .addCase(changeStep, (state, action) => {
+      state.step = action.payload;
+    });
+});
+
+// export const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case increment.type: {
+//       return {
+//         ...state,
+//         counter: state.counter + state.step,
+//       };
+//     }
+//     case decrement.type:
+//       return {
+//         ...state,
+//         counter: state.counter - state.step,
+//       };
+//     case changeStep.type:
+//       return {
+//         ...state,
+//         step: action.payload,
+//       };
+//     case reset.type:
+//       return {
+//         ...state,
+//         step: 1,
+//         counter: 0,
+//       };
+//     default:
+//       return state;
+//   }
+// };
