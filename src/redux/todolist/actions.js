d20f298abce49c7e64c260b1d990646c6288a7d6
Sergey <sergsmile29@gmail.com>
2024-08-20 10:37:32 +0300

    createReducer todo

--- src/redux/todolist/actions.js
+++ src/redux/todolist/actions.js
@@ -1,29 +1,34 @@
-import { ADD_TODO, CHANGE_FILTER, CHANGE_SEARCH, DELETE_TODO } from './constants';
+import { createAction } from '@reduxjs/toolkit';
 
-export const deleteTodo = id => {
-  return {
-    type: DELETE_TODO,
-    payload: id,
-  };
-};
+// export const deleteTodo = id => {
+//   return {
+//     type: DELETE_TODO,
+//     payload: id,
+//   };
+// };
 
-export const addTodo = todo => {
-  return {
-    type: ADD_TODO,
-    payload: todo,
-  };
-};
+// export const addTodo = todo => {
+//   return {
+//     type: ADD_TODO,
+//     payload: todo,
+//   };
+// };
 
-export const changeSearch = searchValue => {
-  return {
-    type: CHANGE_SEARCH,
-    payload: searchValue,
-  };
-};
+// export const changeSearch = searchValue => {
+//   return {
+//     type: CHANGE_SEARCH,
+//     payload: searchValue,
+//   };
+// };
 
-export const changeFilter = filter => {
-  return {
-    type: CHANGE_FILTER,
-    payload: filter,
-  };
-};
+// export const changeFilter = filter => {
+//   return {
+//     type: CHANGE_FILTER,
+//     payload: filter,
+//   };
+// };
+
+export const deleteTodo = createAction('deleteTodo');
+export const addTodo = createAction('addTodo');
+export const changeSearch = createAction('changeSearch');
+export const changeFilter = createAction('changeFilter');
--- src/redux/todolist/reducer.js
+++ src/redux/todolist/reducer.js
@@ -1,4 +1,5 @@
-import { ADD_TODO, CHANGE_FILTER, CHANGE_SEARCH, DELETE_TODO } from './constants';
+import { createReducer } from '@reduxjs/toolkit';
+import { addTodo, changeFilter, changeSearch, deleteTodo } from './actions';
 
 const initialState = {
   items: [{ id: 1, todo: 'Learn Redux', completed: true }],
@@ -6,31 +7,47 @@ const initialState = {
   search: '',
 };
 
-export const todosReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case DELETE_TODO: {
-      return {
-        ...state,
-        items: state.items.filter(item => item.id !== action.payload),
-      };
-    }
-    case ADD_TODO: {
-      return {
-        ...state,
-        items: [...state.items, action.payload],
-      };
-    }
-    case CHANGE_SEARCH:
-      return {
-        ...state,
-        search: action.payload,
-      };
-    case CHANGE_FILTER:
-      return {
-        ...state,
-        filter: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+export const todosReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(deleteTodo, (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    })
+    .addCase(addTodo, (state, action) => {
+      state.items.push(action.payload);
+    })
+    .addCase(changeSearch, (state, action) => {
+      state.search = action.payload;
+    })
+    .addCase(changeFilter, (state, action) => {
+      state.filter = action.payload;
+    });
+});
+
+// export const todosReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case DELETE_TODO: {
+//       return {
+//         ...state,
+//         items: state.items.filter(item => item.id !== action.payload),
+//       };
+//     }
+//     case ADD_TODO: {
+//       return {
+//         ...state,
+//         items: [...state.items, action.payload],
+//       };
+//     }
+//     case CHANGE_SEARCH:
+//       return {
+//         ...state,
+//         search: action.payload,
+//       };
+//     case CHANGE_FILTER:
+//       return {
+//         ...state,
+//         filter: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
