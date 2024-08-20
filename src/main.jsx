05d5721bc5a3a4ab58fc0b907178c7fd28f3ffcd
Sergey <sergsmile29@gmail.com>
2024-08-20 11:56:57 +0300

    redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css';
 import 'modern-normalize';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
     <Toaster />
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,27 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { counterReducer } from './counter/slice';
 import { todoReducer } from './todolist/slice';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'bootcamp',
+  version: 1,
+  storage,
+  // whitelist: ['items', 'search'],
+  blacklist: ['search', 'filter'],
+};
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    todolist: todoReducer,
+    todolist: persistReducer(persistConfig, todoReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
